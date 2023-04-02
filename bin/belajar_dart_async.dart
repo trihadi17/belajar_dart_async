1659e4bf3ea055b05d21c4339ef39f9a0a0ec5a6
conferenceal <appconferenceal@gmail.com>
2023-04-02 11:08:17 +0700

    First Commit

---
+++ bin/belajar_dart_async.dart
@@ -0,0 +1,5 @@
+import 'package:belajar_dart_async/belajar_dart_async.dart' as belajar_dart_async;
+
+void main(List<String> arguments) {
+  print('Hello world: ${belajar_dart_async.calculate()}!');
+}
---
+++ test/belajar_dart_async_test.dart
@@ -0,0 +1,8 @@
+import 'package:belajar_dart_async/belajar_dart_async.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
